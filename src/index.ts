604b3faf914c82bee8ed4e88d4e4c36cb25f1a2c
zkochan <zoltan.kochan@gmail.com>
2017-07-11 22:24:32 +0300

    feat: more strongly typed logs and log messages
    BREAKING CHANGES:
    
    The log name can only be one of the listed ones

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,16 @@
 import bole = require('bole')
 
-export default bole('pnpm')
+const baseLogger = bole('pnpm')
+
+export default baseLogger
+
+export const deprecationLogger = baseLogger('deprecation') as Logger<DeprecationMessage>
+export const rootLogger = baseLogger('root') as Logger<RootMessage>
+export const installCheckLogger = baseLogger('install-check') as Logger<InstallCheckMessage>
+export const progressLogger = baseLogger('progress') as Logger<ProgressMessage>
+export const lifecycleLogger = baseLogger('lifecycle') as Logger<LifecycleMessage>
+export const stageLogger = baseLogger('stage') as Logger<'resolution_done'>
+export const summaryLogger = baseLogger('summary') as Logger<void>
 
 export type LifecycleMessage = {
   pkgId: string,
@@ -35,19 +45,67 @@ export type InstallCheckMessage = {
   pkgId: string,
 }
 
-export type Log = {
-  name: string,
+export type LogBase = {
   level: 'debug' | 'info' | 'warn' | 'error',
 }
 
-export type ProgressLog = Log & ProgressMessage
+export type DeprecationMessage = {
+  pkgName: string,
+  pkgVersion: string,
+  pkgId: string,
+  deprecated: string,
+}
 
-export type LifecycleLog = Log & LifecycleMessage
+export type RootMessage = {
+  added: {
+    name: string,
+    version: string,
+    id: string,
+  },
+} | {
+  removed: {
+    name: string,
+    version?: string,
+  },
+}
 
-export type InstallCheckLog = Log & InstallCheckMessage
+export type StandardMessage = {message: string}
+
+export type ProgressLog = {name: 'pnpm:progress'} & LogBase & ProgressMessage
+
+export type LifecycleLog = {name: 'pnpm:lifecycle'} & LogBase & LifecycleMessage
+
+export type InstallCheckLog = {name: 'pnpm:install-check'} & LogBase & InstallCheckMessage
+
+export type DeprecationLog = {name: 'pnpm:deprecation'} & LogBase & DeprecationMessage
+
+export type StageLog = {name: 'pnpm:stage'} & LogBase & {message: 'resolution_done'}
+
+export type RegistryLog = {name: 'pnpm:registry'} & LogBase & StandardMessage
+
+export type RootLog = {name: 'pnpm:root'} & LogBase & RootMessage
+
+export type SummaryLog = {name: 'pnpm:summary'} & LogBase
+
+export type Log = ProgressLog
+  | LifecycleLog
+  | InstallCheckLog
+  | DeprecationLog
+  | StageLog
+  | RegistryLog
+  | RootLog
+  | SummaryLog
+  | ({name: 'pnpm'} & LogBase)
 
 export type LoggedPkg = {
   rawSpec: string,
   name: string,
   dependentId?: string,
+}
+
+export type Logger<T> = {
+  debug: (log: T) => void,
+  info: (log: T) => void,
+  warn: (log: T) => void,
+  error: (log: T) => void,
 }
