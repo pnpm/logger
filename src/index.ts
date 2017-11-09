ef28a691e774962046af3e6382e3927fb066f578
zkochan <zoltan.kochan@gmail.com>
2017-11-09 22:53:48 +0200

    feat: export logger with type specified

--- src/index.ts
+++ src/index.ts
@@ -13,15 +13,24 @@ bole.setFastTime()
 
 const baseLogger = bole('pnpm')
 
-export default baseLogger
+export default baseLogger as Logger<object | string>
 
 export interface LogBase {
   level: 'debug' | 'info' | 'warn' | 'error',
 }
 
+declare function logError<T>(log?: T): void
+declare function logError<T>(err: Error, log?: T): void
+
+export interface LogError<T> {
+  (err: Error, log?: T): void
+  (log?: T): void
+}
+
 export interface Logger<T> {
-  debug: (log: T) => void,
-  info: (log: T) => void,
-  warn: (log: T) => void,
-  error: (log: T) => void,
+  <Y>(name: string): Logger<Y>,
+  debug: (log?: T) => void,
+  info: (log?: T) => void,
+  warn: (log?: T) => void,
+  error: LogError<T>,
 }
